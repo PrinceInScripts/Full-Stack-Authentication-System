df7dcfc57834cd404610fdad2dcea03343a8e19f
princekumar <princepk7878kumar@gmail.com>
2024-02-10 20:11:02 +0530

    done constant.js

--- server/src/constant.js
+++ server/src/constant.js
@@ -1,3 +1,19 @@
 
 export const DB_NANE="authentication-system"
 
+export const userRolesEnum={
+    SUPER_ADMIN:"SUPER_ADMIN",
+    ADMIN:"ADMIN",
+    MANAGER:"MANAGER",
+    USER:"USER"
+}
+
+export const AvailableUserROles=Object.values(userRolesEnum)
+
+export const userLoginType={
+    GOOGLE:"GOOGLE",
+    GITHUB:"GITHUB",
+    EMAIL_PASSWORD:"EMAIL_PASSWORD"
+}
+
+export const AvailableUserLoginTypes=Object.values(userLoginType)
