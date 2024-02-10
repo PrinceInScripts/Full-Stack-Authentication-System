54fc2a697008d70a291b79a9a32215a29fd7213a
princekumar <princepk7878kumar@gmail.com>
2024-02-10 20:01:05 +0530

    done error middleware and done basic setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';    
 import cookieParser from 'cookie-parser';
+import { errorHandler } from './middlewares/error.middlewares.js';
+import morgan from "morgan"
 
 const app=express();
 
@@ -13,4 +15,5 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+app.use(errorHandler)
 export {app}
