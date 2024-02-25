428420964b23553de0911c27cf8c5dae7e279967
princekumar <princepk7878kumar@gmail.com>
2024-02-25 16:57:57 +0530

    added react-roastify

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 
 function App() {
+
+  useEffect(()=>{
+     toast.error("sucess")
+  },[])
   return (
    <div className="text-blue-500">hoo</div>
   );
