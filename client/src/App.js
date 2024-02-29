47aca1b1111d0977144eade6eb650fd14d71ef71
princekumar <princepk7878kumar@gmail.com>
2024-02-29 10:07:11 +0530

    done contact us compoent

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ function App() {
       <Route path="/about" element={<About/>}/>
       <Route path="/service" element={<Service/>}/>
       <Route path="/contact" element={<Contact/>}/>
+      <Route path="/contactus" element={<ContactComp/>}/>
       
     </Routes>
   );
--- client/src/components/ContactComp/ContactComp.jsx
+++ client/src/components/ContactComp/ContactComp.jsx
@@ -8,15 +8,12 @@ function ContactComp() {
           <div className="bg-center bg-cover w-full h-full absolute top-0 left-0" style={{ backgroundImage: `url(${contact})` }}></div>
           <div className="bg-black opacity-50 w-full h-full absolute top-0 left-0"></div>
           <div className="hero-content flex flex-col w-full lg:w-2/5 text-center text-white relative z-10">
-            <h1 className="text-5xl font-bold mb-6">Hello there</h1>
-            <p className="mb-8 font-sans font-semibold">
-              Welcome to our Full-Stack Authentication System! We are your premier destination for secure and efficient
-              authentication solutions. With our expertise and dedication, we're here to safeguard your data and provide
-              a seamless user experience.
-            </p>
+          <p className="mb-2 font-sans font-semibold">
+           Contact us today to learn more about our services
+          </p>
+            <h1 className="text-5xl font-bold mb-6">Let's work together to make it amazing</h1>
             <div className="flex gap-4">
-              <Link to="/" className="btn btn-primary">Get Started</Link>
-              <Link to="/" className="btn btn-secondary">Learn More</Link>
+              <Link to="/contact" className="btn btn-secondary">Contact Us</Link>
             </div>
           </div>
         </div>
