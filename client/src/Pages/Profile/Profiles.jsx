391e38c6a71dc790e7ee79484487f2847474edee
princekumar <princepk7878kumar@gmail.com>
2024-04-21 10:56:41 +0530

    done

--- client/src/Pages/Profile/Profiles.jsx
+++ client/src/Pages/Profile/Profiles.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from "react-redux"
+import Layout from '../../components/Layout/Layout';
 
 function Profiles() {
+  const user=useSelector((state)=>state?.auth?.user)
+
+  useEffect(()=>{
+   console.log(user);
+  },[])
+    
   return (
-    <div>
-      
-    </div>
+    <Layout>
+      hi
+    </Layout>
   )
 }
 
