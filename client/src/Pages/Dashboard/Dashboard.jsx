88f5f343385d401db05859a9ea3201ac4de21020
princekumar <princepk7878kumar@gmail.com>
2024-05-25 10:21:19 +0530

    done

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -190,7 +190,7 @@ const DashboardOverview = () => {
 
   return (
     <>
-    <div className='bg-base-700 w-full min-h-screen px-40 py-10'>
+    <div className='bg-base-700 w-full min-h-screen px-40 py-10'> 
       <div className='py-10'>
       <h1 className='text-2xl font-bold font-serif'>Dashboard</h1>
       <p className='text-gray-500 font-semibold font-sans'>Details of Users.....</p>
